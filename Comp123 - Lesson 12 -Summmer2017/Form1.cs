77f5f5afeaa464123345813652434b5ad4400c0a
300931676 <300931676@PR-A315-13>
2017-08-01 16:58:59 -0400

    Add project files.

---
+++ Comp123 - Lesson 12 -Summmer2017/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comp123___Lesson_12__Summmer2017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
